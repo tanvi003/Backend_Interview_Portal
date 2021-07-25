544337a874627eaf9bbbace613a97882fdd4733c
Tanvi Mahajan <tanvimahajan@Tanvis-MacBook-Pro.local>
2021-07-25 17:53:18 +0530

    Error fix

--- server.js
+++ server.js
@@ -24,8 +24,28 @@ var smtpTransport = nodemailer.createTransport({
 });
 
 const app = express()
-
-app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+// app.use(cors());
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+  app.use(cors(corsOpts));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
